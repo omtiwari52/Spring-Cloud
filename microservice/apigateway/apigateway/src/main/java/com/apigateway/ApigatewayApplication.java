5e78a4d0e9afd51a8db06d93314fa37cfa89879b
omtiwari52 <tiwariom900@gmail.com>
2025-03-20 02:03:07 +0530

    [Om Tiwari] add: Microservices testing project hystrix added✨

--- microservice/apigateway/apigateway/src/main/java/com/apigateway/ApigatewayApplication.java
+++ microservice/apigateway/apigateway/src/main/java/com/apigateway/ApigatewayApplication.java
@@ -2,13 +2,14 @@ package com.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 		System.out.println("App started");
 	}
-
 }
